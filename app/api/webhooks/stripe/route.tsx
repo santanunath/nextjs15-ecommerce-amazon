507f532bd062e63aadc0640d8834fad2bf225c59
santanunath <43264004+santanunath@users.noreply.github.com>
2025-07-12 13:33:45 +0530

    Update route.tsx

--- app/api/webhooks/stripe/route.tsx
+++ app/api/webhooks/stripe/route.tsx
@@ -6,20 +6,26 @@ import Order from '@/lib/db/models/order.model'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
-export async function POST(req: NextRequest) {
+
+
+export async function POST(req: NextRequest) 
+{
   const event = await stripe.webhooks.constructEvent(
     await req.text(),
     req.headers.get('stripe-signature') as string,
     process.env.STRIPE_WEBHOOK_SECRET as string
   )
 
-  if (event.type === 'charge.succeeded') {
+  if (event.type === 'charge.succeeded') 
+  {
     const charge = event.data.object
     const orderId = charge.metadata.orderId
     const email = charge.billing_details.email
     const pricePaidInCents = charge.amount
     const order = await Order.findById(orderId).populate('user', 'email')
-    if (order == null) {
+    
+    if (order == null) 
+    {
       return new NextResponse('Bad Request', { status: 400 })
     }
 
@@ -31,15 +37,24 @@ export async function POST(req: NextRequest) {
       email_address: email!,
       pricePaid: (pricePaidInCents / 100).toFixed(2),
     }
-    await order.save()
-    try {
+    
+    await order.save() //save on order table
+    
+    try 
+    {
       await sendPurchaseReceipt({ order })
-    } catch (err) {
+    } 
+    catch (err) 
+    {
       console.log('email error', err)
     }
+    
     return NextResponse.json({
       message: 'updateOrderToPaid was successful',
     })
-  }
+    
+  } //outermost endif
+  
   return new NextResponse()
-}
+  
+} //POST()
